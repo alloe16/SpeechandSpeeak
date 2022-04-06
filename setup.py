35c5ee45b9764d19de85e02b823c3c6b2b205de1
alloe16 <101383654+alloe16@users.noreply.github.com>
2022-04-06 20:24:33 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpeechandSpeeak',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zhora',
+    author_email='',
+    description=''
+)
